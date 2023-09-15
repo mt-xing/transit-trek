15c633f57bb7f18e5abc43516173f4a12bdb6981
mt-xing <mt-xing@users.noreply.github.com>
2023-09-14 21:56:09 -0700

    Pray revalidate works

--- src/app/api/public-get/route.ts
+++ src/app/api/public-get/route.ts
@@ -3,6 +3,8 @@ import { DbInfo } from '@/app/types';
 import { kv } from '@vercel/kv';
 import { NextResponse } from 'next/server';
 
+export const revalidate = 1;
+
 export async function GET() {
     const db = await kv.get<DbInfo>(DB_KEY);
     const info = db?.teams?.map(x => ({id: x.id, name: x.name, score: x.score})).sort((a, b) => b.score - a.score);
