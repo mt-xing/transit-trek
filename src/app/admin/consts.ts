3b4984ac5f7ec1b25e95dc33fc980aacbe9e5fb2
mt-xing <mt-xing@users.noreply.github.com>
2023-09-16 16:52:30 -0700

    Hotfix

--- src/app/admin/consts.ts
+++ src/app/admin/consts.ts
@@ -1,3 +1,3 @@
 export const CLIENT_ID = "370521712823-b7258e13jr4un8ahd1v25eqcto2o0hhs.apps.googleusercontent.com";
 
-export const REDIRECT_URI = "https://transit-trek.vercel.app/admin";
+export const REDIRECT_URI = "https://transit-trek-2.vercel.app/admin";
