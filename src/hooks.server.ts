086615837c27a8196654753d27e02f152040b4db
mt-xing <mt-xing@users.noreply.github.com>
2024-01-21 16:48:45 -0800

    What is happening

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,14 +5,12 @@ import { redirect, type Handle, error } from '@sveltejs/kit';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import { isValidAdmin } from './utils/auth/verify';
 
-const adminStringLength = '/admin/'.length;
-
 const authorization: Handle = async ({ event, resolve }) => {
 	// Protect any routes under /admin
 	if (event.url.pathname.startsWith('/admin')) {
 		const session = await event.locals.getSession();
 		if (!session || !session.user) {
-			const remainingUrl = event.url.pathname.substring(adminStringLength);
+			const remainingUrl = event.url.pathname.substring(7);
 			throw redirect(303, `/auth/${remainingUrl}`);
 		} else if (!isValidAdmin(session.user)) {
 			throw error(401, "No Arjun Allowed");
