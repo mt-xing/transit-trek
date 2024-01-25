cae175c5c2271f7a85e77c34eedea2a6a1873c34
mt-xing <mt-xing@users.noreply.github.com>
2024-01-24 18:21:39 -0800

    Early rules

--- src/routes/tt3/+page.svelte
+++ src/routes/tt3/+page.svelte
@@ -28,15 +28,76 @@
 	</div>
 </div>
 
-<!-- <h1>
-	<span class="firstLine">Race Across</span><br />
-	<span class="secondLine">Seattle</span>
-</h1> -->
+<main>
+	<p>
+		You are at Northgate Station to eat some yummy Chinese food. You soon realize you are actually
+		yearning for the delicious food of Seattle’s historical Chinatown. You’re out of money, but by
+		completing tasks, you can earn passes to take segments of public transit to make it all the way
+		there by dinnertime.
+	</p>
+
+	<h2>Requirements</h2>
+	<ul>
+		<li>
+			ORCA card with either a pass or at least $6 loaded on it
+			<br /><small>Microsoft- and Amazon-issued ORCA cards suffice for this requirement</small>
+		</li>
+		<li>
+			A smartphone
+			<br /><small>One team member must have an iPhone with Find My Friends enabled</small>
+		</li>
+		<li>Joined the Signal group</li>
+		<li>
+			Either:
+			<ul>
+				<li>A bandana from Seattle Transit Trek #2, or</li>
+				<li>
+					$0.50 participation fee. In return, you will be given a bandana to be used throughout the
+					game (and to keep as a souvenir!)
+				</li>
+			</ul>
+		</li>
+	</ul>
+	<p>Optional but recommended:</p>
+	<ul>
+		<li>Sunscreen</li>
+		<li>Water</li>
+		<li>Battery pack (for your phone)</li>
+		<li>Snacks, if you might get hungry during the event</li>
+		<li>
+			A change of clothes or a towel, if you are preparing for the possibility of a task involving
+			water
+		</li>
+	</ul>
+
+	<h2>Basics</h2>
+	<ul>
+		<li>To take public transit, you have to complete certain tasks of varying difficulty</li>
+		<li>Most tasks are relatively close to public transit stations</li>
+		<li>3 to 4 people per team</li>
+		<li>4 hours long competition period</li>
+	</ul>
+</main>
 
 <style>
 	:global(body) {
 		padding: 0;
 		margin: 0;
+	}
+
+	main {
+		background: rgba(0, 0, 0, 0.75);
+		backdrop-filter: blur(10px);
+		box-shadow: 0 0 15px 10px black;
+
+		width: 90%;
+		max-width: 1500px;
+		margin: 15vh auto 12.5vh auto;
+		color: white;
+		padding: 5vh 10vh;
+		box-sizing: border-box;
+		font-family: 'Segoe UI', 'Helvetica', sans-serif;
+		font-size: 20px;
 	}
 
 	.textWrap {
