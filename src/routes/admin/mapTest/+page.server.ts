83791e10b6ead9c87f08329e018d1dcec83627e8
mt-xing <mt-xing@users.noreply.github.com>
2024-05-11 13:36:44 -0700

    Team Page Reflect Time

--- src/routes/admin/mapTest/+page.server.ts
+++ src/routes/admin/mapTest/+page.server.ts
@@ -2,6 +2,8 @@ import { CosmosClient } from '@azure/cosmos';
 import type { PageServerLoad } from './$types';
 import { DB_URL, READ_KEY } from '$env/static/private';
 import type { ChallengeDefinition } from '../../../types/challenge';
+import { GAME_KEY, type GameState } from '../../../types/game';
+import type { Team } from '../../../types/team';
 
 const client = new CosmosClient({
 	endpoint: DB_URL,
@@ -9,14 +11,43 @@ const client = new CosmosClient({
 });
 
 export const load: PageServerLoad = async () => {
-	const res2 = await client
+	const challengeRes = await client
 		.database('transit-trek')
 		.container('challenges')
 		.items.readAll<ChallengeDefinition>()
 		.fetchAll();
 
+	const gameStateRes = await client
+		.database('transit-trek')
+		.container('game')
+		.item(GAME_KEY, GAME_KEY)
+		.read<GameState>();
+
 	return {
-		allChallenges: res2.resources,
-		startTime: new Date().getTime(),
+		allChallenges: challengeRes.resources,
+		gameState: gameStateRes.resource,
+		team: {
+			id: 'abcd',
+			teamNum: 1,
+			secret: 'xyz',
+			name: '',
+			timePenaltyMin: 10,
+			challengeProgress: {
+				'b2afe19c-d245-4461-89e2-d373371e354a': { progress: [true] },
+				'a84efa50-22d3-4ff9-860f-c4f010756fdc': { progress: [true], manualUnlock: true },
+				'3b914c8f-e728-41d5-a142-b27f19aba575': { progress: [true] },
+				'843bbbb0-3ab1-417a-a1b4-a69bb06ec7af': { progress: [true, true, true] },
+				'08e2fc05-fca2-4f35-90ca-94e959d6571f': {
+					progress: [true, true],
+					manualUnlock: true,
+					manualComplete: false,
+				},
+				'6aed52ef-df4f-4bba-9fac-8aed85ce04b2': {
+					manualUnlock: true,
+					progress: undefined,
+				},
+			},
+			finishTime: 1715468137161,
+		} as Team,
 	};
 };
--- src/routes/admin/mapTest/+page.svelte
+++ src/routes/admin/mapTest/+page.svelte
@@ -3,26 +3,12 @@
 	import ChallengeView from '../../../components/challengeView.svelte';
 	import Map from '../../../components/map/map.svelte';
 	import type { PublicChallengeDefinition } from '../../../types/challenge';
+	import assertUnreachable from '../../../utils/assertUnreachable';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	const { allChallenges, startTime } = data;
-	const challengeProgress = {
-		'b2afe19c-d245-4461-89e2-d373371e354a': { progress: [true] },
-		'a84efa50-22d3-4ff9-860f-c4f010756fdc': { progress: [true], manualUnlock: true },
-		'3b914c8f-e728-41d5-a142-b27f19aba575': { progress: [true] },
-		'843bbbb0-3ab1-417a-a1b4-a69bb06ec7af': { progress: [true, true, true] },
-		'08e2fc05-fca2-4f35-90ca-94e959d6571f': {
-			progress: [true, true],
-			manualUnlock: true,
-			manualComplete: false,
-		},
-		'6aed52ef-df4f-4bba-9fac-8aed85ce04b2': {
-			manualUnlock: true,
-			progress: undefined,
-		},
-	};
-	const teamPenalty = 10;
+	const { allChallenges, gameState, team } = data;
+	const { challengeProgress, timePenaltyMin } = team;
 
 	let selectedChallenge: null | PublicChallengeDefinition = null;
 
@@ -34,11 +20,27 @@
 		return x;
 	}
 	function updateTimeString() {
-		const d = new Date().getTime();
-		const delta = d - startTime;
+		switch (gameState?.t) {
+			case undefined:
+			case 'pre':
+				timeString = '00:00:00';
+				return;
+			case 'post':
+			case 'ongoing':
+				break;
+			default:
+				assertUnreachable(gameState);
+		}
+
+		const d = gameState.t === 'ongoing' ? new Date().getTime() : team.finishTime;
+		if ((gameState.t === 'post' && !team.finishTime) || !d) {
+			timeString = 'Did Not Finish';
+			return;
+		}
+		const delta = d - gameState.startTime + team.timePenaltyMin * 60 * 1000;
 
 		if (delta < 0) {
-			timeString = 'Game has not started yet';
+			timeString = '00:00:00';
 		} else {
 			const hours = Math.floor(delta / 1000 / 60 / 60);
 			const rawMins = delta - hours * 60 * 60 * 1000;
@@ -62,11 +64,11 @@
 </script>
 
 <svelte:head>
-	<title>Transit Trek: Team 0 Dashboard</title>
+	<title>Transit Trek: Team {team.teamNum} Dashboard</title>
 	<meta name="description" content="Seattle Transit Trek's May 2024 event, Race Across Seattle" />
 </svelte:head>
 
-<h1><em>Test Team</em><br />Team 0 Dashboard</h1>
+<h1><em>{team.name || 'Untitled Team'}</em><br />Team {team.teamNum} Dashboard</h1>
 
 <div class="card">
 	<p>This page is private and should only be viewed by members of your team.</p>
@@ -79,10 +81,17 @@
 <div class="card">
 	<h2>Time</h2>
 	<p class="timeString">{timeString}</p>
-	{#if teamPenalty > 0}
-		<p>+{teamPenalty} minute penalty</p>
-	{:else if teamPenalty < 0}
-		<p>-{-1 * teamPenalty} minute handicap</p>
+	{#if gameState?.t === 'ongoing' || gameState?.t === 'post'}
+		{#if timePenaltyMin > 0}
+			<p>(includes +{timePenaltyMin} minute penalty)</p>
+		{:else if timePenaltyMin < 0}
+			<p>(includes {timePenaltyMin} minute handicap)</p>
+		{/if}
+	{:else}
+		<p>Game has not started yet</p>
+	{/if}
+	{#if gameState?.t === 'post'}
+		<p>Game has concluded.</p>
 	{/if}
 </div>
 
