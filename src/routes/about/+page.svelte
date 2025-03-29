9b95014bffe7489c1d450e0686c2a2bbd156e17f
Jeffrey Wang <jeffw16@users.noreply.github.com>
2025-03-28 17:37:31 -0700

    Update new timeline

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -103,8 +103,10 @@
 		<p>
 			We tend to finalize the date and the theme of the game about one month ahead, but sometimes we
 			might <s>procrastinate</s> be a bit more pressed for time, depending on the situation. Around this
-			time, you'll need to sign up for the Transit Trek. The cutoff to sign up will usually be one week
-			before the day of the Trek. This gives us enough time to create and assign teams.
+			time, you'll need to sign up for the Transit Trek. To help us create and assign teams better, we
+			highly recommend signing up at least one week before the Trek begins, but as a courtesy to those
+			who find time to join at the last minute, we allow same-day sign-ups (potentially with a late
+			registration fee).
 		</p>
 		<p>
 			The full list of challenges will only be released when the game starts at 2pm on the day of
@@ -113,7 +115,7 @@
 	</div>
 	<div class="q">
 		<p class="question">How accessible is the game?</p>
-		<p>We try to make the game as accessible as possible. For example:</p>
+		<p>We try to make the game as accessible as possible, within reason. For example:</p>
 		<ul>
 			<li>
 				You don't need to come prepared with your own team. We're happy to match you into a team!
@@ -182,10 +184,11 @@
 	<div class="q">
 		<p class="question">Can I get outside help?</p>
 		<p>
-			Using Google Maps, a search engine, or a chatbot is usually allowed unless expressly
+			Using Google Maps, a search engine, or an AI chatbot is usually allowed unless expressly
 			prohibited.
 		</p>
 		<p>Intentional communication with other teams and colluding are prohibited.</p>
+		<p>You may not communicate with people who are not participating in the game for their help.</p>
 	</div>
 	<div class="q">
 		<p class="question">Are there places I cannot go?</p>
