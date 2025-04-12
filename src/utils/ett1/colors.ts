842d0717b2928e47cc8b51c96dc329c78ce26956
mt-xing <mt-xing@users.noreply.github.com>
2025-04-11 23:12:36 -0400

    More colors

--- src/utils/ett1/colors.ts
+++ src/utils/ett1/colors.ts
@@ -26,6 +26,16 @@ export function getColor(teamNum: number) {
             return "#84B817";
         case 13:
             return "#8480d7";
+        case 14:
+            return "#93ceba";
+        case 15:
+            return "#00bd19";
+        case 16:
+            return "#000000";
+        case 17:
+            return "#00BD19";
+        case 18:
+            return "#b727bf";
         default:
             return "hotpink";
     }
