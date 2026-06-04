c0b2bac74ceac10f1292d497d50fdf16820aceb4
mt-xing <mt-xing@users.noreply.github.com>
2026-06-04 14:04:10 -0400

    Add KaTeX

---
+++ src/components/katex.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import katex from 'katex';
+	import 'katex/dist/katex.min.css';
+
+	export let math;
+	export let displayMode = false;
+
+	const options = {
+		displayMode,
+		throwOnError: false,
+	};
+
+	$: katexString = katex.renderToString(math, options);
+</script>
+
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+{@html katexString}
