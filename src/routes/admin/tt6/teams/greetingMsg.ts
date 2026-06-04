468bb6100ae25cccc6b931741cdcd36e79a955e3
mt-xing <mt-xing@users.noreply.github.com>
2026-06-03 22:49:47 -0400

    Team greeting

--- src/routes/admin/tt6/teams/greetingMsg.ts
+++ src/routes/admin/tt6/teams/greetingMsg.ts
@@ -6,11 +6,11 @@ Note that this URL is specific to your team and should be kept secret, so please
 
 On your dashboard, once the game starts, you will be able to see all the challenges available to you, your team's score, rank, and other important progress information.
 
-Please know that this is effectively the same sketchy site from last year which I have not had time to refactor or improve in any meaningful way, so I apologize in advance if it breaks or goes down during the game. Fingers crossed it'll be okay though.
+Please know that this site was built back in 2023 before AI and is composed of 100% artisanal, hand-crafted garbage code, so I apologize in advance if it breaks or goes down during the game. Fingers crossed it'll be okay though.
 
 Finally, your first task is to come up with a team name, so if you have not yet already, please decide amongst yourselves what you want your name to be. Just let me know once you have it.
 
-Best of luck, and have fun!`;
+Best of luck, team ${teamNum}, and have fun!`;
 
 	navigator.clipboard.writeText(text).then(() => {
 		// eslint-disable-next-line no-alert
