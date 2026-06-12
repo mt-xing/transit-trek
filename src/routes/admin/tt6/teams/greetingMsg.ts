84cd59598a3e43ee94c653a86aa4e35d4e9a0914
mt-xing <mt-xing@users.noreply.github.com>
2026-06-12 18:42:31 -0400

    Rules tweak

--- src/routes/admin/tt6/teams/greetingMsg.ts
+++ src/routes/admin/tt6/teams/greetingMsg.ts
@@ -1,5 +1,6 @@
 export function copyGreeting(teamSecretGuid: string, teamNum: number) {
-	const text = `Hi y'all! I'm Michael, and I'll be running Game Control today. Please ping me in this chat when you complete challenges so I can check off your progress. You can also ask me questions and I'll try my best to answer or direct you to someone who can.
+	const text =
+		`Hi y'all! Welcome to the Crosslake Crossover, team ${teamNum}! I'm Michael, and I'll be running Game Control today. Please ping me in this chat when you complete challenges so I can check off your progress. If a challenge has multiple parts, please ping me as you complete each part (don't wait until the end). You can also ask me questions and I'll try my best to answer or direct you to someone who can.
 
 Your team's dashboard is available at this link: http://transittrek.org/tt6/game?id=${teamSecretGuid}
 Note that this URL is specific to your team and should be kept secret, so please don't share it with anyone outside your team. If your URL is accidentally leaked, please let me know immediately as I can invalidate and generate a new link for you.
@@ -10,7 +11,7 @@ Please know that I built this site back in 2023 before AI and it's thus composed
 
 Finally, your first task is to come up with a team name, so if you have not yet already, please decide amongst yourselves what you want your name to be. Just let me know once you have it.
 
-Best of luck, team ${teamNum}, and have fun!`;
+Best of luck, and have fun!`;
 
 	navigator.clipboard.writeText(text).then(() => {
 		// eslint-disable-next-line no-alert
