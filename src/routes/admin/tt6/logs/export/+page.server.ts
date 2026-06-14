31e88bb2db147014ce7d850bab3f1bcfd9a8dd52
mt-xing <mt-xing@users.noreply.github.com>
2026-06-14 12:18:09 -0400

    Export Functionality

---
+++ src/routes/admin/tt6/logs/export/+page.server.ts
@@ -0,0 +1,35 @@
+import { CosmosClient } from '@azure/cosmos';
+import { error } from '@sveltejs/kit';
+import { DB_URL, READ_KEY } from '$env/static/private';
+import type { PageServerLoad } from './$types';
+import { type TT6ChallengeDefinition } from '../../../../../types/tt6/challenge';
+import type { TT6Team } from '../../../../../types/tt6/team';
+
+const client = new CosmosClient({
+	endpoint: DB_URL,
+	key: READ_KEY,
+});
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const session = await locals.getSession();
+	if (!session) {
+		throw error(401, 'Forbidden');
+	}
+
+	const [res, teamRes] = await Promise.all([
+		client
+			.database('transit-trek')
+			.container('tt6-challenges')
+			.items.query({query: 'SELECT * FROM c ORDER BY c.sort ASC'})
+			.fetchAll(),
+		client.database('transit-trek')
+			.container('tt6-teams')
+			.items.readAll<TT6Team>()
+			.fetchAll(),
+	]);
+
+	return {
+		challenges: res.resources as TT6ChallengeDefinition[],
+		teams: teamRes.resources,
+	};
+};
---
+++ src/routes/admin/tt6/logs/export/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const { challenges, teams } = data;
+	if (!challenges || !teams) {
+		throw new Error();
+	}
+</script>
+
+<h1>Challenges</h1>
+
+<textarea readonly>{JSON.stringify(challenges, null, 2)}</textarea>
+
+<h1>Teams</h1>
+
+<textarea readonly>{JSON.stringify(teams, null, 2)}</textarea>
+
+<p><a href="/admin">Back</a></p>
+
+<style>
+	textarea {
+		width: 400px;
+		height: 500px;
+	}
+</style>
